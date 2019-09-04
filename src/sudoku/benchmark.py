8154fd4b7835f1409b66186b79cc2ffdce4e87c3
Omar Rampado <rampado.omar@gmail.com>
2019-09-04 15:35:55 +0200

    multiproc

--- src/sudoku/benchmark.py
+++ src/sudoku/benchmark.py
@@ -34,6 +34,30 @@ def from_csv(csv_filename, total_rows=10_000):
 
 	return error_rows, counter
 
+def from_csv_parallel(csv_filename, total_rows=10_000):
+	import csv
+	from concurrent import futures
+	from tqdm import tqdm
+
+	chuck_tqdm = max(total_rows // 1000, 1)
+	counter = 0
+	error_rows = []
+	with open(csv_filename, 'r') as csvfile:
+		reader = csv.DictReader(csvfile)
+		with futures.ProcessPoolExecutor() as executor:
+			exec_map = executor.map(_resolve, reader)
+
+			with tqdm(total=total_rows, mininterval=5) as pbar:
+				for correct, row in exec_map:
+					counter += 1
+					if not correct:
+						error_rows.append(row)
+
+					if counter % chuck_tqdm == 0:
+						pbar.update(chuck_tqdm)
+
+	return error_rows, counter	
+
 if __name__ == '__main__':
 	import sys
 	import time
@@ -42,7 +66,7 @@ if __name__ == '__main__':
 	start = time.time()
 
 	# args: filename, number of rows
-	errors, rows = from_csv(sys.argv[1], int(sys.argv[2]))
+	errors, rows = from_csv_parallel(sys.argv[1], int(sys.argv[2]))
 
 	end = time.time()
 	elaps = end - start
